8dcc5fd81ef856f5e302f88ac2fd9646519f7fa0
OmarMWarraich <earthling986@gmail.com>
2022-11-28 11:12:29 +0500

    feat: Set landing_page as home route
    feat: Bypass static_pages in routes
    feat: Link privacy_policy to root_path via Home
    feat: Link home page to privacy_policy_path bypassing static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-
+  root "static_pages#landing_page"
+  # get 'static_pages/landing_page'
+  # get 'static_pages/privacy_policy'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
